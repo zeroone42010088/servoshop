93bcf0ae13f29cf26e65c1fe46549ed8155aad37
Глеб <stroikagoda@yandex.ru>
2018-07-26 17:01:57 +0400

    Продолжаю делать магазин

--- eshop.js
+++ eshop.js
@@ -1,4 +1,6 @@
-$('document').ready(function(){
+var cart = {}; //моя корзина
+
+$('document').ready(function () {
     loadGoods();
 });
 
@@ -8,8 +10,21 @@ function loadGoods() {
         //console.log(data);
         var out = '';
         for (var key in data) {
-            out+='<p>'+data
+            out += '<div class="single-goods">';
+            out += '<h3>' + data[key]['name'] + '</h3>';
+            out += '<p>Цена: ' + data[key]['cost'] + '</p>';
+            out += '<img src="' + data[key].image + '">';
+            out += '<button class="add-to-cart" data-art="' + key + '">Купить</button>'
+            out += '</div>';
         }
-        
-    })
+        $('#goods').html(out);
+        $('button.add-to-cart').on('click', addToCart);
+    });
+}
+
+function addToCart() {
+    //добавляем товар в корзину
+    var articul = $(this).attr('data-art');
+    cart[articul] = 1;
+    console.log(cart);
 }
