268dcea432a79882da997c5586b10f85b64e5ba5
Глеб <stroikagoda@yandex.ru>
2018-07-27 17:35:12 +0400

    Найстрока кодировки главной страницы

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>Title</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
