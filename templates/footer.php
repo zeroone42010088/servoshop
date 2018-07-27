4616fd957ba513582f9df6918102441b9d83f5b8
Глеб <stroikagoda@yandex.ru>
2018-07-27 17:49:35 +0400

    Добавляю элементы родительского сайта

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,67 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ </div>
+    <!-- .wrapper -->
 
-<script src="js/cart.js"></script>
+    <script>
+        (function(d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter44532427 = new Ya.Metrika({
+                        id: 44532427,
+                        clickmap: true,
+                        trackLinks: true,
+                        accurateTrackBounce: true,
+                        webvisor: true
+                    });
+                } catch (e) {}
+            });
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function() {
+                    n.parentNode.insertBefore(s, n);
+                };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else {
+                f();
+            }
+        })(document, window, "yandex_metrika_callbacks");
 
-<?php
-switch ($route) {
-    case '':
-        echo '<script src="js/eshop.js"></script>';
-        break;
-    case "cart":
-       echo '<script src="js/cart.js"></script>';
-            break;   
-}
-?>
+    </script> <noscript><div><img src="../https@mc.yandex.ru/watch/44532427" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 
-    </body>
+    <!-- BEGIN JIVOSITE CODE {literal} -->
+    <script>
+        (function() {
+            var widget_id = 'ON5DROotFX';
+            var d = document;
+            var w = window;
 
-    </html>
+            function l() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = '//code.jivosite.com/script/widget/' + widget_id;
+                var ss = document.getElementsByTagName('script')[0];
+                ss.parentNode.insertBefore(s, ss);
+            }
+            if (d.readyState == 'complete') {
+                l();
+            } else {
+                if (w.attachEvent) {
+                    w.attachEvent('onload', l);
+                } else {
+                    w.addEventListener('load', l, false);
+                }
+            }
+        })();
+
+    </script>
+    <!-- {/literal} END JIVOSITE CODE -->
+
+    
+</body>
+
+</html>
